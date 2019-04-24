97543715ab9e0b549e5edf60ed5348683086fd14
guoxiaoyu <guoxiaoyu@lvzheng.com>
2019-04-24 15:50:37 +0800

    妥协老jebe流程添加监听器

---
+++ flowengine-service/src/main/java/com/jx/service/workflow/engine/listener/common/CommonCreateListener.java
@@ -0,0 +1,14 @@
+package com.jx.service.workflow.engine.listener.common;
+
+import org.activiti.engine.delegate.DelegateTask;
+import org.activiti.engine.delegate.TaskListener;
+
+public class CommonCreateListener implements TaskListener {
+
+	private static final long serialVersionUID = 1L;
+	@Override
+	public void notify(DelegateTask delegateTask) {
+
+	}
+
+}
---
+++ flowengine-service/src/main/java/com/jx/service/workflow/engine/listener/common/CommonFinishListener.java
@@ -0,0 +1,15 @@
+package com.jx.service.workflow.engine.listener.common;
+
+import org.activiti.engine.delegate.DelegateTask;
+import org.activiti.engine.delegate.TaskListener;
+
+public class CommonFinishListener implements TaskListener {
+
+	private static final long serialVersionUID = 1L;
+	@Override
+	public void notify(DelegateTask delegateTask) {
+
+		
+	}
+
+}
